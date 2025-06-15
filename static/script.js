b4c26f56410556731c2fbfdfb48c49e1e100af24
claryssayuanwie <claryuanwie@gmail.com>
2025-06-15 02:57:05 -0400

    update

--- static/script.js
+++ static/script.js
@@ -43,6 +43,7 @@ const factDetails = {
   interests: `
     <h4>Interests</h4>
     <p>I am passionate about using tech — especially AI — to improve fields like business and education. I've worked at startups and published <a href="https://papers.ssrn.com/sol3/papers.cfm?abstract_id=4989842" style = "text-decoration: none;"><strong style="color: var(--accent-color);">research</strong></a> on how we can continue to develop current practices.</p>
+    <p> I also love connecting with transfer students and joining student clubs!</p>
   `
 };
 
